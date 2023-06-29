aebd86228ed73e75b43eba716fd54c84872f9fdd
Nilodirf <106656383+Nilodirf@users.noreply.github.com>
2023-06-29 22:59:23 +0200

    update plot_input
    Completed the documentation of the current simulation and visualization status

--- new_M3TM/plot_input.py
+++ new_M3TM/plot_input.py
@@ -1,7 +1,11 @@
 from plot import SimPlot
+# Here you can call some plot functions, plot and save plots
 
+# Initialize the plot class with the simulation results folder denoted 'file':
 plotter = SimPlot(file='try_1')
 
+# Plot a map of the denoted simulation of one of the three subsystems, save if you want to:
 plotter.map_plot(key='mag', save_fig=False)
 
+# Plot the dynamics of one subsystem for some layers in line-plots to see the dynamics, save if you want to:
 plotter.line_plot(key='tp', min_layer=0, max_layer=10, save_fig=False)
