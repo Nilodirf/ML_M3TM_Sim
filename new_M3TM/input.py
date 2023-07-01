7c952e479ea13eef07e6f3f04c23398d6e150b1d
Nilodirf <106656383+Nilodirf@users.noreply.github.com>
2023-07-01 17:44:58 +0200

    useless push

--- new_M3TM/input.py
+++ new_M3TM/input.py
@@ -11,7 +11,6 @@ from mainsim import SimDynamics
 
 
 # Create the necessary materials. For documentation of the parameters see mats.sim_materials class:
-
 cgt = SimMaterials(name='CGT', pen_dep=10e-9, tdeb=175, dz=20.5e-10, vat=1e-29, ce_gamma=650., cp_max=8.9e6,
                    kappap=5., kappae=0.01, gep=15e16, spin=1.5, tc=65., muat=4., asf=0.3)
 sio2 = SimMaterials(name='SiO2', pen_dep=1, tdeb=403, dz=5.4e-10, vat=1e-28, ce_gamma=0., cp_max=4e6, kappap=14.,
