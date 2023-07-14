3f73409d53f9e451f907d0fce58e18951295e022
Nilodirf <106656383+Nilodirf@users.noreply.github.com>
2023-07-14 18:21:16 +0200

    modify timegrid

--- new_M3TM/plot_input.py
+++ new_M3TM/plot_input.py
@@ -8,9 +8,11 @@ plotter = SimPlot(file='15_nm')
 
 # Plot a map of the denoted simulation of one of the three subsystems, save if you want to:
 # plotter.map_plot(key='tp', max_layer=30, max_time=30, save_fig=True)
-# plotter.map_plot(key='te', max_time=30, save_fig=True)
+# plotter.map_plot(key='te', max_time=30, save_fig=False)
+# plotter.map_plot(key='tp', max_time=30, max_layer=30, save_fig=False)
+
 
 # Plot the dynamics of one subsystem for some layers in line-plots to see the dynamics, save if you want to:
-# plotter.line_plot(key='tp', average=False, min_layer=13, max_layer=20, save_fig=True)
+plotter.line_plot(key='tp', max_layer=30, average=False, save_fig=False)
 # plotter.line_plot(key='tp', average=False, min_layer=0, max_layer=30, save_fig=True)
 plotter.line_plot(key='mag', min_layer=0, max_layer=2, average=True)
