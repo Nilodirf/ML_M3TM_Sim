e3256815d955dedf2152b6fe9a6c9e03b0651ec2
Nilodirf <106656383+Nilodirf@users.noreply.github.com>
2024-09-20 16:32:09 +0200

    Update fgt_analysis.py

--- code/Plot/fgt_analysis.py
+++ code/Plot/fgt_analysis.py
@@ -3,22 +3,37 @@ import os
 from matplotlib import pyplot as plt
 
 
-class FGTSim:
+def get_data(self, subsys):
 
-    def __init__(self, file):
-        self.file = file
+    sim_files_folder = 'Results/FGT/' + self.file + '/'
+    delay = np.load(sim_files_folder + 'delays.npy')
+    te = np.load(sim_files_folder + 'tes.npy')
+    tp = np.load(sim_files_folder + 'tps.npy')
+    mag = np.load(sim_files_folder + 'ms.npy')
 
-        return
+    if os.path.isfile(sim_files_folder + 'tp2s.npy'):
+        tp2 = np.load(sim_files_folder + 'tp2s.npy')
+    else:
+        tp2 = np.zeros_like(tp)
 
-    def get_data(self):
+    return delay, te, tp, tp2, mag
 
-        sim_files_folder = 'Results/FGT/' + self.file + '/'
-        delay = np.load(sim_files_folder + 'delays.npy')
-        te = np.load(sim_files_folder + 'tes.npy')
-        tp = np.load(sim_files_folder + 'tps.npy')
-        mag = np.load(sim_files_folder + 'ms.npy')
 
-        if os.path.isfile(sim_files_folder + 'tp2s.npy'):
-            tp2 = np.load(sim_files_folder + 'tp2s.npy')
-        else:
-            tp2 = np.zeros_like(tp)
+def create_figure():
+
+    fig, axs = plt.subplots(nrows=1, ncols=3)
+    axs[0].set_x_label(r'delay [ps]', fontsize=16)
+    axs[0].set_ylabel(r'$m/m_0$', fontsize=16)
+    axs[1].set_ylabel(r'$T_p$', fontsize=16)
+    axs[2].set_ylabel(r'$T_e$', fontsize=16)
+
+    axs[0].set_xlim(0, 10)
+    axs[1].set_xlim(0, 10)
+    axs[2].set_xlim(0, 10)
+
+    fig.tight_layout()
+
+    return fig, axs
+
+def plot_te()
+
