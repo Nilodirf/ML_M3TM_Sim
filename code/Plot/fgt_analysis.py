6e3ed81c8d0354fa0f31ed6c3dac0f9b840d2cc9
Nilodirf <106656383+Nilodirf@users.noreply.github.com>
2024-10-07 17:31:37 +0200

    FGT initial timescale fit
    fitting process seems to work well for therm_time and gamma

--- code/Plot/fgt_analysis.py
+++ code/Plot/fgt_analysis.py
@@ -1,6 +1,7 @@
 import numpy as np
 import os
 from matplotlib import pyplot as plt
+from matplotlib import cm
 from scipy import io
 
 from ..Source.finderb import finderb
@@ -11,16 +12,16 @@ def get_data(file, subsys):
     sim_files_folder = 'Results/FGT/' + file + '/'
     delay = np.load(sim_files_folder + 'delay.npy')
     if subsys == 'te':
-        val_1 = np.load(sim_files_folder + 'tes.npy')
+        val_1 = np.load(sim_files_folder + 'tes.npy')[:, 0]
         val_2 = None
     elif subsys == 'tp':
-        val_1 = np.load(sim_files_folder + 'tps.npy')
+        val_1 = np.load(sim_files_folder + 'tps.npy')[:, 0]
         if os.path.isfile(sim_files_folder + 'tp2s.npy'):
-            val_2 = np.load(sim_files_folder + 'tp2s.npy')
+            val_2 = np.load(sim_files_folder + 'tp2s.npy')[:, 0]
         else:
             val_2 = None
     elif subsys == 'mag':
-        val_1 = np.load(sim_files_folder + 'ms.npy')
+        val_1 = np.load(sim_files_folder + 'ms.npy')[:, 0]
         val_1 = val_1/val_1[0]
         val_2 = None
     else:
@@ -30,21 +31,61 @@ def get_data(file, subsys):
     return delay*1e12-1, val_1, val_2
 
 
+def get_te_exp():
+    f = io.loadmat('input_data/FGT/exp_data/Temperatures.mat')
+
+    exp_delay = f['Delay'] / 1e3
+
+    f1 = f['F0p6'][:, 0]
+    # f2 = f['F1p2']
+    # f3 = f['F1p8']
+    # f4 = f['F2p5']
+    # f5 = f['F3p0']
+    df1 = f['Err_F0p6'][:, 0]
+    # df2 = f['Err_F1p2']
+    # df3 = f['Err_F1p8']
+    # df4 = f['Err_2p5']
+    # df5 = f['Err_F3p0']
+
+    return exp_delay, f1, df1
+
+
+def get_mag_exp():
+    exp_dat = np.loadtxt('input_data/FGT/exp_data/mag.txt')
+    exp_delay = exp_dat[:, 0]
+    exp_mag = np.copy(exp_dat[:, 1])
+    time_zero_index = finderb(0, exp_delay)[0]
+    exp_mag = exp_mag / exp_mag[time_zero_index]
+    exp_dmag = exp_dat[:, 2] / exp_dat[time_zero_index, 1]
+
+    return exp_delay, exp_mag, exp_dmag
+
+
+def get_msd_exp():
+    exp_dat = np.loadtxt('input_data/FGT/exp_data/MSD_SEP_24.txt')
+    exp_delay = exp_dat[:, 0] - 0.7
+    exp_msd = np.copy(exp_dat[:, 1])
+    exp_msd /= np.amax(exp_msd)
+    exp_dmsd = exp_dat[:, 2] / np.amax(exp_dat[:, 1])
+
+    return exp_delay, exp_msd, exp_dmsd
+
+
 def create_figure():
 
-    fig, axs = plt.subplots(nrows=3, ncols=1, figsize=(8,6))
+    fig, axs = plt.subplots(nrows=3, ncols=1, figsize=(8, 6))
 
     axs[2].set_xlabel(r'delay [ps]', fontsize=16)
 
     axs[0].set_ylabel(r'$T_e$', fontsize=16)
     axs[1].set_ylabel(r'$m/m_0$', fontsize=16)
-    axs[2].set_ylabel(r'$T_p$', fontsize=16)
+    axs[2].set_ylabel(r'MSD', fontsize=16)
 
-    axs[0].set_xlim(-0.05, 0.6)
-    axs[1].set_xlim(-0.1, 5)
-    axs[2].set_xlim(-0.1, 5)
+    axs[0].set_xlim(-0.05, 0.5)
+    axs[1].set_xlim(-0.1, 10)
+    axs[2].set_xlim(-0.1, 10)
 
-    axs[1].set_ylim(0.6, 1.05)
+    axs[1].set_ylim(0.5, 1.05)
 
     fig.tight_layout()
 
@@ -56,20 +97,7 @@ def plot_te(file, figure, axs, show_exp=True):
     axs[0].plot(delay, te, color='orange')
 
     if show_exp:
-        f = io.loadmat('input_data/FGT/exp_data/Temperatures.mat')
-
-        exp_delay = f['Delay']/1e3
-
-        f1 = f['F0p6'][:, 0]
-        # f2 = f['F1p2']
-        # f3 = f['F1p8']
-        # f4 = f['F2p5']
-        # f5 = f['F3p0']
-        df1 = f['Err_F0p6'][:, 0]
-        # df2 = f['Err_F1p2']
-        # df3 = f['Err_F1p8']
-        # df4 = f['Err_2p5']
-        # df5 = f['Err_F3p0']
+        exp_delay, f1, df1 = get_te_exp()
         axs[0].errorbar(exp_delay, f1, yerr=df1, fmt='o', color='orange')
 
     return figure, axs
@@ -81,11 +109,7 @@ def plot_mag(file, figure, axs, show_exp=True):
     axs[1].plot(delay, mag, color='green')
 
     if show_exp:
-        exp_dat = np.loadtxt('input_data/FGT/exp_data/mag.txt')
-        exp_delay = exp_dat[:, 0]-0.1992
-        exp_mag = exp_dat[:, 1]
-        exp_mag = exp_mag/exp_mag[0]+0.1
-        exp_dmag = exp_dat[:, 2]/0.017715
+        exp_delay, exp_mag, exp_dmag = get_mag_exp()
         axs[1].errorbar(exp_delay, exp_mag, yerr=exp_dmag, fmt='o', color='green')
 
     return figure, axs
@@ -118,11 +142,7 @@ def plot_tp(file, figure, axs, show_exp=True):
         axs[2].plot(delay, ep_tot_norm, color='blue')
 
     if show_exp:
-        exp_dat = np.loadtxt('input_data/FGT/exp_data/MSD_SEP_24.txt')
-        exp_delay = exp_dat[:, 0]-0.7
-        exp_msd = exp_dat[:, 1]
-        exp_msd /= np.amax(exp_msd)
-        exp_dmsd = exp_dat[:, 2]/np.amax(exp_msd)
+        exp_delay, exp_msd, exp_dmsd = get_msd_exp()
         axs[2].errorbar(exp_delay, exp_msd, yerr=exp_dmsd, fmt='o', color='blue')
 
     return figure, axs
@@ -133,15 +153,68 @@ def show_plot(figure, axs):
 
     return
 
+
 def save_plot(figure, axs, name):
     plt.savefig(name)
 
     return
 
 
-fig, axs = create_figure()
-fig, axs = plot_te(file='fits_new/te_tt_15fs', figure=fig, axs=axs)
-fig, axs = plot_tp(file='fits_new/tp_tt_15fs', figure=fig, axs=axs)
-fig, axs = plot_mag(file='fits_new/mag_tt_15fs', figure=fig, axs=axs)
-save_plot(fig, axs, 'bad_fit_to_will.pdf')
-show_plot(fig, axs)
+def init_fit():
+    # Find the optimal parameters for \gamma and therm_time from fitting the initial electron temperature increase
+
+    files = os.listdir('Results/FGT/fits_init')
+    gammas = np.arange(180, 241)
+    therm_times = np.arange(0, 41)
+    chi_sq = np.zeros((41, 61))
+    for file in files:
+        full_path = 'fits_init/' + file
+        therm_time = float(file[:file.find('_')])
+        gamma = int(file[file.find('_')+1:])
+
+        therm_time_index = finderb(therm_time, therm_times)[0]
+        gamma_index = finderb(gamma, gammas)[0]
+
+        delay, te, bla = get_data(full_path, 'te')
+        exp_delay, exp_te, exp_dte = get_te_exp()
+
+        max_fit_delay_index = finderb(0.045, exp_delay)[0]+3
+        fit_delay = exp_delay[:max_fit_delay_index]
+
+        delay_indices = finderb(fit_delay, delay)
+        cs = np.sum(((exp_te[:max_fit_delay_index]-te[delay_indices])/exp_dte[:max_fit_delay_index])**2)
+        cs_norm = cs/len(delay_indices)*np.sum(exp_dte[:max_fit_delay_index]**(-2))
+
+        chi_sq[therm_time_index, gamma_index] = cs_norm
+
+        # if therm_time == 15.0:
+        #     plt.scatter(exp_delay[:max_fit_delay_index], exp_te[:max_fit_delay_index], color='blue')
+        #     plt.scatter(delay[delay_indices], te[delay_indices], color='red', label=str(gamma))
+        #     plt.legend()
+        #     plt.show()
+
+    therm_times, gammas = np.meshgrid(therm_times, gammas)
+
+    fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+    surf = ax.plot_surface(therm_times, gammas, chi_sq.T, cmap=cm.coolwarm,
+                           linewidth=0, antialiased=False)
+    fig.colorbar(surf, shrink=0.5, aspect=5)
+    plt.show()
+
+    return
+
+
+def show_fits(save, show):
+    fig, axs = create_figure()
+    fig, axs = plot_te(file='fits_new/te_tt_15fs', figure=fig, axs=axs)
+    fig, axs = plot_tp(file='fits_new/tp_tt_15fs', figure=fig, axs=axs)
+    fig, axs = plot_mag(file='fits_new/mag_tt_15fs', figure=fig, axs=axs)
+    if save:
+        save_plot(fig, axs, 'bad_fit_to_will.pdf')
+    if show:
+        show_plot(fig, axs)
+
+        return
+
+
+init_fit()
--- code/Scripts/FGT_input.py
+++ code/Scripts/FGT_input.py
@@ -10,7 +10,9 @@ from code.Source.pulse import SimPulse
 from code.Source.mainsim import SimDynamics
 
 to_fit = ['te', 'mag', 'tp']
+# Fluence with pen_dep=1nm; Te:9.8e-3, mag:49e-3, Tp:38e-3
 fluences = [9.8e-3, 49e-3, 38e-3]
+# Starting tempratures; Te:100K, mag:25K, Tp:100K
 ini_temps = [100., 25., 100.]
 
 for subsys, flu, initemp in zip(to_fit, fluences, ini_temps):
@@ -19,9 +21,9 @@ for subsys, flu, initemp in zip(to_fit, fluences, ini_temps):
 
     FGT = SimMaterials(name='Fe3GeTe2', cp_max=None, cp_method='input_data/FGT/FGT_c_p1.txt', tdeb=232.,  kappap=0.,
                        ce_gamma=210., gep=4.7e17,
-                       asf=0.01, spin=2, tc=232., vat=127.76e-30, muat=1.6)
+                       asf=0.018, spin=2, tc=232., vat=127.76e-30, muat=1.6)
 
-    FGT.add_phonon_subsystem(gpp=1.9e17, cp2_max=None, cp2_method='input_data/FGT/FGT_c_p2.txt')
+    FGT.add_phonon_subsystem(gpp=2.5e17, cp2_max=None, cp2_method='input_data/FGT/FGT_c_p2.txt')
 
     # Create a sample, then add desired layers of the materials you want to simulate.
     # The first material to be added will be closest to the laser pulse and so on.
@@ -29,12 +31,10 @@ for subsys, flu, initemp in zip(to_fit, fluences, ini_temps):
     sample.add_layers(material=FGT, layers=1,  dz=1.7e-9, pen_dep=1e-9)
 
     # Create a laser pulse with the desired parameters. (Fluence in mJ/cm^2)
-    # Fluence with pen_dep=1nm; Te:9.8e-3, mag:49e-3, Tp:38e-3
     pulse = SimPulse(sample=sample, method='LB', pulse_width=15e-15, fluence=flu, delay=1e-12, therm_time=1.5e-14)
     # pulse.visualize(axis='t')
 
     # Initialize the simulation with starting temperature and final time, the solver to be used and the maximum timestep:
-    # Starting tempratures; Te:100K, mag:25K, Tp:100K
     sim = SimDynamics(sample=sample, pulse=pulse, end_time=11e-12, ini_temp=initemp, solver='RK45', max_step=1e-13)
 
     # Run the simulation by calling the function that creates the map of all three baths
